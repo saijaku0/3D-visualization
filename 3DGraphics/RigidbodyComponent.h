c62013a78f7d839acc737631ffec439a82923732
saijaku0 <minerking112256@gmail.com>
2025-12-08 22:25:19 +0100

    Add impulse function

--- 3DGraphics/RigidbodyComponent.h
+++ 3DGraphics/RigidbodyComponent.h
@@ -8,6 +8,7 @@ public:
     glm::vec3 velocity = glm::vec3(0.0f);
     float mass = 1.0f;
     float drag = 0.4f;
+    float restitution = 0.0f;
 
     bool isStatic = false;
     bool useGravity = true;
@@ -18,6 +19,7 @@ public:
     virtual ~RigidbodyComponent() = default;
 
     void AddForce(const glm::vec3 force);
+    void AddImpulse(const glm::vec3& impulse);
 
     void Stop();
 };
