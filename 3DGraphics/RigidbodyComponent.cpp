b36d6ce774e3da5c1f7ffe4e144cd3fea0463c2e
saijaku0 <minerking112256@gmail.com>
2025-12-08 22:25:02 +0100

    Add impulse function

--- 3DGraphics/RigidbodyComponent.cpp
+++ 3DGraphics/RigidbodyComponent.cpp
@@ -6,6 +6,12 @@ void RigidbodyComponent::AddForce(const glm::vec3 force) {
 	velocity += force / mass;
 }
 
+void RigidbodyComponent::AddImpulse(const glm::vec3& impulse) {
+	if (isStatic) return;
+
+	velocity += impulse / mass;
+}
+
 void RigidbodyComponent::Stop() {
 	velocity = glm::vec3(0.0f);
 }
