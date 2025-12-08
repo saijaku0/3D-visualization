b3ee3fe67ba5f60bd1b12bd724a7218646284bcf
saijaku0 <minerking112256@gmail.com>
2025-12-08 19:26:09 +0100

    Update logic

--- 3DGraphics/AttachedCamera.h
+++ 3DGraphics/AttachedCamera.h
@@ -8,14 +8,22 @@
 class AttachedCamera : public Camera {
     const GameObject* target;
     glm::vec3 offset;
+    float m_distance;
 public:
     AttachedCamera(GameObject* targetObj, glm::vec3 offsetVal = glm::vec3(0.0f, 0.8f, 0.0f))
         : target(targetObj), offset(offsetVal)
     {
+        m_distance = glm::length(offsetVal);
+
+        m_Yaw = -90.0f;
+        m_Pitch = -20.0f;
+
         Update(0.0f);
     }
 
     void Update(float deltaTime) override;
+
+    void ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch = true) override;
 
     void SetTarget(const GameObject* newTarget) { target = newTarget; }
 };
